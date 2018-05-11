87aeefe04d476ca101b820513027b04e2f8a55b7
Maxim Nechaev <nechaev_maxim@mail.ru>
2018-05-12 01:36:29 +0300

    Update sms_to_email.pl

--- sms_to_email.pl
+++ sms_to_email.pl
@@ -1,18 +1,20 @@
 #!/usr/bin/perl -w
 use strict;
 use utf8;
+binmode(STDOUT,':utf8');
 
-use LWP::UserAgent;
-use JSON;
 use Data::Dumper;
 use Encode;
+use DateTime;
+use DateTime::Format::Mail;
+use LWP::UserAgent;
+use JSON;
+use MIME::Entity;
 use Email::Sender::Simple qw(sendmail);
 use Email::Sender::Transport::SMTP::TLS;
-use Email::Simple;
-use Email::Simple::Creator;
 
-
-my $modem      = '192.168.99.1';
+my $modem      = 'ip.ip.ip.ip';
+my $phone      = '+7916NNNNNNN';
 my $emailInfo  = 'info@example.com';
 my $sms_list   = "http://$modem/goform/goform_get_cmd_process?isTest=false&cmd=sms_data_total&page=0&data_per_page=500&mem_store=1&tags=10&order_by=order+by+id+desc";
 my $sms_delete = "http://$modem/goform/goform_set_cmd_process";
@@ -20,28 +22,37 @@ my $sms_delete = "http://$modem/goform/goform_set_cmd_process";
 my $ua = LWP::UserAgent->new;
 my $req_list = HTTP::Request->new(GET => $sms_list);
 my $resp = $ua->request($req_list);
-
 my $body = $resp->decoded_content;
-my $messages = decode_json $body;
-my $sms = $messages->{messages}->[0];
-my $content = $sms->{content};
-my $content_decoded = decode('UCS-2', pack 'H*', $content);
-$sms->{content_decoded} = $content_decoded;
-
-my $email = Email::Simple->create(
-    header => [
-        To      => $emailInfo,
-        From    => $emailInfo,
-        Subject => "[SMS] $sms->{number}",
-    ],
-    body => "Text: $sms->{content_decoded}\n",
+my $json = decode_json $body;
+my $sms = $json->{messages}->[-1];
+$sms->{text} = decode('UCS-2', pack 'H*', $sms->{content});
+my ($year, $month, $day, $hour, $minute, $second) = split ',', $sms->{'date'};
+$sms->{datetime} = DateTime->new(
+    year      => 2000 + $year,
+    month     => $month,
+    day       => $day,
+    hour      => $hour,
+    minute    => $minute,
+    second    => $second
 );
+
+my $email = MIME::Entity->build(
+    Type     => 'text/plain',
+    Encoding => 'quoted-printable',
+    Charset  => 'UTF-8',
+    Date     => DateTime::Format::Mail->format_datetime( $sms->{datetime} ),
+    To       => encode('MIME-Header', "$phone <$emailInfo>"),
+    From     => encode('MIME-Header', "$sms->{number} <$emailInfo>"),
+    Subject  => encode('MIME-Header', "[SMS]"),
+    Data     => encode('UTF-8', $sms->{text})
+);
+
 my $transport = Email::Sender::Transport::SMTP::TLS->new(
-    host => 'smtp.gmail.com',
-    port => 587,
+    host     => 'smtp.gmail.com',
+    port     => 587,
     username => $emailInfo,
     password => 'password',
-    helo => 'somehost,
+    helo     => 'example.com'
 );
 sendmail($email, { transport => $transport });
 
@@ -51,7 +62,7 @@ $resp = $ua->post(
         isTest      => 'false',
         goformId    => 'DELETE_SMS',
         msg_id      => "$sms->{id};",
-        notCallback => 'true',
+        notCallback => 'true'
     }
 );
 
