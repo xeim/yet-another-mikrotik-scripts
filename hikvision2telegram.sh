82af1f69f5d7fa92cbaf1adbe8443bf50b964841
Maxim Nechaev <nechaev_maxim@mail.ru>
2019-05-27 16:46:29 +0300

    Create hikvision2telegram.sh

---
+++ hikvision2telegram.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# get picture from hikvision camera
+CAMERA="ip.ip.ip.ip"
+USER="some_user"
+PASS="some_pass"
+PIC=`tempfile -p campic`
+curl \
+    --digest --user $USER:$PASS \
+    --output $PIC \
+    --silent \
+    http://$CAMERA/Streaming/channels/101/picture
+
+# send picture to Telegram chat
+BOT="botNNNNNNNNN:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+CHAT="NNNNNNNNN"
+curl \
+    --request POST \
+    --form chat_id=$CHAT \
+    --form photo=@$PIC \
+    --silent \
+    https://api.telegram.org/$BOT/sendPhoto
+
+# remove picture
+rm $PIC
