6d301785ffa94cd7c8ee8fb7e53dd23f521680f4
Maxim Nechaev <nechaev_maxim@mail.ru>
2019-06-14 16:11:59 +0300

    add archive clean

--- hikvision_record.sh
+++ hikvision_record.sh
@@ -3,13 +3,18 @@
 CAMERA="ip.ip.ip.ip"
 USER="some_user"
 PASS="some_pass"
-DURATION=607
+DURATION=300
+SAVE=40
 
-ROOT=/srv/rec/`date +%Y`/`date +%Y-%m`/`date +%F`
-RECORD=$ROOT/`date +%H%M_%m%d`.mp4
+BASE=/srv/rec
+ROOT=$BASE/`date +%Y`/`date +%Y-%m`/`date +%F`
 mkdir -p $ROOT
+RECORD=$ROOT/`date +%H%M_%m%d`.mp4
 
 cvlc \
     rtsp://$USER:$PASS@$CAMERA:554/Streaming/Channels/102 \
     --sout='#transcode{vcodec=mjpg,fps=8,scale=1}:std{access=file,mux=mp4,dst='$RECORD'}' \
     --run-time=$DURATION --stop-time=$DURATION vlc://quit 2>/dev/null
+
+find $BASE/ -type f -mtime +$SAVE -delete
+find $BASE/ -type d -empty -delete
