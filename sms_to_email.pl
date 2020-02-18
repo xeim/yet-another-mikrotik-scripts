10f600bf5934178bff88624376462ec6ae732d64
Maxim Nechaev <nechaev_maxim@mail.ru>
2020-02-18 14:17:24 +0300

    Try handle partial SMS
    Wait up to 3 script runs

--- sms_to_email.pl
+++ sms_to_email.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl -w
+# Get one oldest SMS from ZTE MF825A (MTS 830FT) modem by HTTP
+# Send SMS to email and delete SMS from modem
+
 use strict;
 use utf8;
 binmode(STDOUT,':utf8');
 
-use Data::Dumper;
 use Encode;
 use Sys::Syslog;
 use DateTime;
@@ -12,6 +14,7 @@ use LWP::UserAgent;
 use JSON;
 use MIME::Entity;
 use Email::Sender::Simple qw(sendmail);
+use Storable;
 
 my $modem      = '192.168.99.1';
 my $phone      = '+7916NNNNNNN';
@@ -19,6 +22,7 @@ my $emailInfo  = 'info@example.com';
 my $timeZone   = 'UTC';
 my $sms_list   = "http://$modem/goform/goform_get_cmd_process?isTest=false&cmd=sms_data_total&page=0&data_per_page=500&mem_store=1&tags=10&order_by=order+by+id+desc";
 my $sms_delete = "http://$modem/goform/goform_set_cmd_process";
+my $tmpfile    = '/tmp/sms_sZFNfR2eOH';
 
 my $ua = LWP::UserAgent->new;
 my $req_list = HTTP::Request->new(GET => $sms_list);
@@ -26,9 +30,21 @@ my $resp = $ua->request($req_list);
 my $body = $resp->decoded_content;
 my $json = decode_json $body;
 exit if !scalar @{ $json->{messages} };
-
 my $sms = $json->{messages}->[-1];
-exit if !$sms->{received_all_concat_sms};
+
+my $stor = eval { retrieve $tmpfile };
+if (!$sms->{received_all_concat_sms} && ++$stor->{count} <= 3) {
+    store $stor, $tmpfile;
+
+    openlog('sms_to_email.pl', 'pid');
+    syslog('info', 'Partial SMS in modem %s from %s found. Waiting %s', $modem, $sms->{number}, $stor->{count});
+    closelog();
+
+    exit;
+} else {
+    unlink $tmpfile;
+}
+
 $sms->{text} = encode('UTF-8', decode('UCS-2', pack 'H*', $sms->{content}) );
 my ($year, $month, $day, $hour, $minute, $second) = split ',', $sms->{'date'};
 $sms->{datetime} = DateTime->new(
@@ -53,7 +69,7 @@ my $email = MIME::Entity->build(
     To       => encode('MIME-Header', $phone        ) . " <$emailInfo>",
     From     => encode('MIME-Header', $sms->{number}) . " <$emailInfo>",
     Subject  => encode('MIME-Header', '[SMS]'),
-    Data     => $sms->{text}
+    Data     => $sms->{text},
 );
 sendmail($email);
 
@@ -63,6 +79,6 @@ $resp = $ua->post(
         isTest      => 'false',
         goformId    => 'DELETE_SMS',
         msg_id      => "$sms->{id};",
-        notCallback => 'true'
+        notCallback => 'true',
     }
 );
