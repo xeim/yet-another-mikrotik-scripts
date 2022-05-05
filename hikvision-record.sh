f74b7c3733b8fc328ae52127bc2b8f5a8225fcd3
Maxim Nechaev <11213682+xeim@users.noreply.github.com>
2022-05-05 18:51:34 +0300

    Update hikvision-record.sh
    delete old records

--- hikvision-record.sh
+++ hikvision-record.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-CAMERA=url
+CAMERA=rtsp://url
 DURATION=1801
-SAVE=22
+SAVE=33d
 
-BASE=yandex:/rec/`date +%Y`/`date +%Y-%m`/`date +%F`
+REMOTE=yandex:/rec
+BASE=`date +%Y`/`date +%Y-%m`/`date +%F`
 RECORD=/tmp/`date +%H%M_%m%d`
 
 ffmpeg -t $DURATION -loglevel error -i $CAMERA -vcodec copy $RECORD.mp4
 mv $RECORD.mp4 $RECORD
+rclone move $RECORD $REMOTE/$BASE --create-empty-src-dirs
 
-rclone move $RECORD $BASE --create-empty-src-dirs
+rclone --min-age $SAVE delete $REMOTE --rmdirs
